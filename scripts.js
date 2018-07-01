0b05ae6af21426418116efe1059ac8bd68a772f0
ste ste <steelst0@gmail.com>
2018-06-30 21:06:08 -0700

    breaking js into it's own file

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// DISAPPEAR A PICTURE WHEN IT IS CLICKED UPON
+$("#picture1 ").click( 
+function (){
+    $("#picture1").hide("#picture1");
+});
+$("#picture2").click( 
+function (){
+    $("#picture2").hide("#picture2");
+});
+$("#picture3").click( 
+function (){
+    $("#picture3").hide("#picture3");
+});
+$("#picture4").click( 
+function (){
+    $("#picture4").hide("#picture4");
+});
+$("#picture5").click( 
+function (){
+    $("#picture5").hide("#picture5");
+});
+$("#picture6").click( 
+function (){
+    $("#picture6").hide("#picture6");
+});
+$("#picture7").click( 
+function (){
+    $("#picture7").hide("#picture7");
+});
+$("#picture8").click( 
+function (){
+    $("#picture8").hide("#picture8");
+});
+// THIS RESTORES US BACK TO ORIGINAL NINJA POWER
+$("#button").click( 
+function (){
+    $("#picture1").show("#picture1");
+    $("#picture2").show("#picture2");
+    $("#picture3").show("#picture3");
+    $("#picture4").show("#picture4");
+    $("#picture5").show("#picture5");
+    $("#picture6").show("#picture6");
+    $("#picture7").show("#picture7");
+    $("#picture8").show("#picture8");
+});
